17f6c46ed1a5550f4f1d96cd547f80cddd07040e
Alex Webb <alexwebbb@protonmail.com>
2018-11-02 05:54:18 -0500

    lil bit more, getting guesses

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	// introduce the game
 	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows! Its a word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
-	cout << " letter word I'm thinking of?";
+	cout << " letter word I'm thinking of?\n";
+
+	// get guess from user
+	string Guess = "";
+	cin >> Guess;
+
+	// repeat the guess back to user
+	cout << "Your guess is " << Guess << ".\n";
+
 }
