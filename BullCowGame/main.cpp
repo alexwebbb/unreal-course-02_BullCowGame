72b7e59a36cbba66a04ac0b98ce0ba83202bf015
Alex Webb <alexwebbb@protonmail.com>
2018-11-02 04:53:05 -0500

    initial commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	constexpr int WORD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows! Its a word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter word I'm thinking of?";
+}
